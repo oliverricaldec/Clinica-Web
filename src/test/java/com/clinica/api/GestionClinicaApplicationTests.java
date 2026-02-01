435b0cc5c37d15bffaafab5c3aec42813fc209d6
Oliver Ricalde <oliverricaldec@gmail.com>
2026-02-01 01:06:21 -0500

    Initial Spring Boot project setup

---
+++ src/test/java/com/clinica/api/GestionClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
