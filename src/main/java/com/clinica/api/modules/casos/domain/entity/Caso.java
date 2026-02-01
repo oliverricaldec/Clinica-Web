9b729091e3379a52d5ce780569991f76bbfb7eb0
Oliver Ricalde <oliverricaldec@gmail.com>
2026-02-01 12:50:33 -0500

    #2 add domain entities (Paciente, HistoriaClinica, Caso, Registro)

---
+++ src/main/java/com/clinica/api/modules/casos/domain/entity/Caso.java
@@ -0,0 +1,51 @@
+package com.clinica.api.modules.casos.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "casos"
+)
+public class Caso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "nombre_caso")
+    private String nombreCaso;
+
+    @Column(nullable = false)
+    private String diagnostico;
+
+    @Column(name = "plan_tratamiento")
+    private String planTratamiento;
+
+    @Column(name = "examen_auxiliar")
+    private String examenAuxiliar;
+
+    @Column(name = "proforma_url")
+    private String proforma;
+
+    @Column(name = "odontograma_url")
+    private String odontograma;
+
+    @Column(nullable = false, name = "costo_total")
+    private BigDecimal costoTotal;
+
+    @Column(nullable = false, name = "fecha_inicio")
+    private LocalDate fechaInicio;
+
+    @Column(nullable = false, name = "fecha_fin")
+    private LocalDate fechaFin;
+
+    @Column(nullable = false, name = "estado_caso")
+    private Estado estado;
+}
---
+++ src/main/java/com/clinica/api/modules/registros/domain/entity/Registro.java
@@ -0,0 +1,39 @@
+package com.clinica.api.modules.registros.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(
+        name = "casos"
+)
+public class Registro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "fecha_atencion")
+    private LocalDate fechaAtencion;
+
+    @Column(nullable = false)
+    private String evolucion;
+
+    @Column(name = "procedimiento_realizado")
+    private String proceRealizado;
+
+    @Column(nullable = false, name = "doctor_responsable")
+    private String doctor;
+
+    @Column(nullable = false, name = "monto_abonado")
+    private BigDecimal abono;
+
+    @Column(name = "observaciones")
+    private String observaciones;
+}
